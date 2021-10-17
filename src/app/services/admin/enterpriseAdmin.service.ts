0700caf83898bf8bbb2a0875fbecf5c31eca8f97
Alejandro <janodevg@outlook.cl>
2021-10-17 20:29:16 -0300

    servicios para empresa

--- src/app/services/admin/enterpriseAdmin.service.ts
+++ src/app/services/admin/enterpriseAdmin.service.ts
@@ -1,43 +1,52 @@
 import {Injectable} from '@angular/core';
 
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {response} from '../../models/account';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnterpriseAdminService {
 
-  url = 'http://localhost:8081/api/';
+  private url = 'http://localhost:8081/api/';
 
   constructor(private http: HttpClient) {
   }
 
-  headersCreateEnterprise(body: any){
-
-  }
-
-  headersCreateEnterprise(body: any){
-
-  }
-
-  createHeaders(body: any) {
+  headersCreateEnterprise(body: response, clietnSecret: String) {
     let headers = {
       'Content-Type': 'application/json',
-      'Xmail': form.Xmail.toString(),
-      'Xpassword': window.btoa(unescape(encodeURIComponent(form.Xpassword.toString()))), // encriptado en Base64
-      'Access-Control-Allow-Origin': '*'
-    }
+      'Xoption': 'CREATE',
+      'Xmail':  String(body.body.mail),
+
+    };
     let requestOptions = {
       headers: new HttpHeaders(headers),
-    }
-    return requestOptions
-
+    };
+    return requestOptions;
   }
 
-  loginByEmail(form: UserLogin) {
-    let direccion = this.url + 'login';
-    return this.http.get<response>(direccion, this.createHeaders(form));
+  headersDeleteEnterprise(body: response, enterpriseName: String) {
+    let headers = {
+      'Content-Type': 'application/json',
+      'Xoption': 'DELETE',
+      'Xmail':  String(body.body.mail),
+      'XenterpriseName': String(enterpriseName),
+    };
+    let requestOptions = {
+      headers: new HttpHeaders(headers),
+    };
+    return requestOptions;
+  }
 
+  createEnterprise(form: response, enterpriseName: String) {
+    let direccion = this.url + 'admin/enterprise';
+    return this.http.post<Response>(direccion, form, this.headersCreateEnterprise(form, enterpriseName));
+  }
+
+  deleteEnterprise(form: response, enterpriseName: String) {
+    let direccion = this.url + 'admin/enterprise';
+    return this.http.delete<Response>(direccion, this.headersDeleteEnterprise(form, enterpriseName));
   }
 
 }
